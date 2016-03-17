4ea550612d8013ff6522bdf21fe6fa68ba19cee9
maiziex <maiziex@gmail.com>
2016-03-17 15:45:51 -0700

    read_indel_addcriterion

--- read_INDEL_addcriterion.py
+++ read_INDEL_addcriterion.py
@@ -55,7 +55,10 @@ def load_indel_all():
 def main():
     pdb.set_trace()
     indel_all = load_indel_all()
-    data = {"x":[], "y":[],"z":[]}
+    data = {"x":[], "y":[],"z":[],"yz":[]}
+    dict1={}
+    dict2={}
+    dict3={}
     all_lenofalt = []
     f = open('indel_vcf_addcriterion_raw','w')
     for label, coord in indel_all.items():
@@ -65,19 +68,76 @@ def main():
         	data["x"].append(int(coord[ii][1]))    #count
 		data["y"].append(coord[ii][0])   #indel
 		data["z"].append(int(coord[ii][2]))  # samplenum
+		data["yz"].append([coord[ii][0],int(coord[ii][2])])  # indel and samplenum
 	
-	indices, L_sorted = zip(*sorted(enumerate(data["x"]), key=itemgetter(1)))
-        lenofalt = len(indices)-1
-        all_lenofalt.append(lenofalt)
-        for jj in reversed(indices):
-		#print label, data["y"][jj], data["x"][jj]		
-		f.write(data["y"][jj])
-		f.write("\t")
-		f.write(str(data["x"][jj]))
-		f.write("\t")		
- 	f.write(os.linesep)	
-	data = {"x":[], "y":[],"z":[]}	
+	for jj in range(len(data["x"])):
+		if data["y"][jj] in dict1.keys():
+			dict1[data["y"][jj]]+= data["x"][jj]
+		else:
+			dict1[data["y"][jj]] = data["x"][jj]
+
+	for kk in range(len(data["x"])):
+		if data["yz"][kk] in dict2.keys():
+			dict2[str(data["yz"][kk])].append(data["x"][kk])
+		else:
+			dict2[str(data["yz"][kk])] = [data["x"][kk]]
+
+	for nn in range(len(data["x"])):
+		if data["y"][nn] in dict3.keys():
+			dict3[str(data["y"][nn])].append([data["z"][nn]])
+		else:
+			dict3[str(data["y"][nn])] = [[data["z"][nn]]]
+
+
+	#if len(dict1)>2:
+	#	print "here"
+
+        d_view = [ (v,k) for k,v in dict1.iteritems() ]
+	d_view.sort(reverse=True) # natively sort tuples by first element
+	#for v,k in d_view:
+    		#print("There are " + str(v) + " " + k)
 	
+       
+       
+        alt_count = 1
+        countp = 1
+	for v,k in d_view:
+		if countp < 3:
+			#print label, data["y"][ll], data["x"][ll]		
+			f.write(k)
+			f.write("\t")
+			f.write(str(v))
+			f.write("\t")
+		else:
+			#print "here"	
+			if len(dict3[k]) > 1:    # two individual has the variant
+				for mm in range(len(dict3[k])):			             
+					count = dict2[str([k]+dict3[k][mm])]
+					if count[0] > 1:      # at least 2 reads for each individual
+						count = [0]
+						continue
+					else:
+						break
+						                 
+					f.write(k)
+					f.write("\t")
+					f.write(str(v))
+					f.write("\t")
+					alt_count = alt_count + 1
+					
+
+
+		countp =  countp +1
+		
+	all_lenofalt.append(alt_count)		
+ 	f.write(os.linesep)
+
+	
+	data = {"x":[], "y":[],"z":[],"yz":[]}	
+	dict1={}
+        dict2={}
+	dict3={}
+	d_view =[]
 
     f.close()	
     print all_lenofalt
